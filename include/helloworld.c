1b2cf98e4959fd2f88faf66e92e7383207956981
i92ruroa <i92ruroa@uco.es>
2020-04-16 16:15:16 +0200

    Initial commit

---
+++ include/helloworld.c
@@ -0,0 +1,10 @@
+#include "./include/myinclude.h"
+int main(){
+           f();
+return 0; }
+y un fichero “include/myinclude.h” que contenga:
+#include <stdio.h>
+void f()
+{
+printf(“Hello World \n”);
+}
