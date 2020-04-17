61cb80d1515c7115848420a8c7ee17d533b1534b
i92ruroa <i92ruroa@uco.es>
2020-04-17 10:23:55 +0200

    Now with strcat()

--- include/helloworld.c
+++ include/helloworld.c
@@ -1,10 +1,7 @@
-#include "./include/myinclude.h"
-int main(){
-           f();
-return 0; }
-y un fichero “include/myinclude.h” que contenga:
-#include <stdio.h>
-void f()
-{
-printf(“Hello World \n”);
+función f() con lo necesario para que f() quede así:
+void f() {
+     char c1[100]= "Hello world";
+     char c2[100]= “, I am <your name here>”;
+     printf("%s\n", strcat(c1, c2));
+     return 0;
 }
